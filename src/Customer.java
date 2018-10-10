d1fd1603a483463a2c98116af3a6d7d873a8de64
TK Lee <tklee1127@gmail.com>
2018-10-09 20:34:21 -0400

    Extract method
    Extract method for long method with complex logic

--- src/Customer.java
+++ src/Customer.java
@@ -26,22 +26,10 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
+
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            thisAmount = getAmount(each);
+
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -60,5 +48,28 @@ public class Customer {
                 +
                 " frequent renter points";
         return result;
+    }
+
+    public double getAmount (Rental each){
+
+        double thisAmount = 0;
+
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+
+        return thisAmount;
     }
 }
