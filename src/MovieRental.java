e857843df3bdf655b1970a34d3a2f982151c5780
TK Lee <tklee1127@gmail.com>
2018-10-09 20:24:16 -0400

    Created main method
    created test method

---
+++ src/MovieRental.java
@@ -0,0 +1,24 @@
+public class MovieRental {
+
+    public static void main (String[] args){
+
+        /*Stock movies*/
+        Movie mov1 = new Movie ("Notebook", 0);
+        Movie mov2 = new Movie ("Avengers", 1);
+        Movie mov3 = new Movie ("Play time", 2);
+
+        /*Register nen*/
+        Customer customer1 = new Customer("TK");
+
+        /*Start Rental*/
+        Rental rentActivity1 = new Rental (mov1, 3);
+        Rental rentActivity2 = new Rental (mov2, 5);
+
+        customer1.addRental(rentActivity1);
+        customer1.addRental(rentActivity2);
+
+
+
+
+    }
+}
