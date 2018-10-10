fc405c073cc4709ee7883476c21106f1640417e6
TK Lee <tklee1127@gmail.com>
2018-10-10 00:01:56 -0400

    Replace Temp with Query for TotalRequentRenterPoints

--- src/Customer.java
+++ src/Customer.java
@@ -20,13 +20,11 @@ public class Customer {
 
     public String statement() {
 
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
 
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -35,7 +33,7 @@ public class Customer {
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalAmount()) +
                 "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)
+        result += "You earned " + String.valueOf(getFrequentRenterPoints())
                 +
                 " frequent renter points";
         return result;
@@ -49,6 +47,18 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
             result += each.getAmount();
         }
+        return result;
+    }
+
+    private int getFrequentRenterPoints(){
+        int result = 0;
+
+        Enumeration rentals = _rentals.elements();
+        while(rentals.hasMoreElements()){
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+
         return result;
     }
 
